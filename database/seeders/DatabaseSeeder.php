3a59002a456d1d0a8b0c03f6448a21db6a85ed91
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-02-21 14:53:25 +0700

    panha Feb21 #2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Fruit;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Fruit::create(
+            [
+                'id' => 1,
+                'name' => 'Apple',
+                'descriptions' => 'Crisp and refreshing, our juicy apples are bursting with natural sweetness. Grown in the fertile orchards of the USA, each bite offers a delightful blend of tartness and succulence.',
+                'price' => 1.99,
+                'origin' => 'USA',
+                'url' => 'https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_1280.jpg',
+            ]
+
+            );
     }
 }
